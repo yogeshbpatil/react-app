952cb21fff658092d18b5cc2cec357415b973c3a
yogeshbpatil <patilyogeshkumarb@gmail.com>
2024-02-26 09:20:29 +0530

    passing children 3.11

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
+      <Alert>
+        Hello <span>World</span>
+      </Alert>
     </div>
   );
 }
+
 export default App;
