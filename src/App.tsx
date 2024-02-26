add7d078d1b2ff9ea78166123e8014ce9d98c48b
yogeshbpatil <patilyogeshkumarb@gmail.com>
2024-02-26 14:37:14 +0530

    Building Button Component 3.13

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import Alert from "./components/Alert";
+import Button from "./components/Button";
+
 function App() {
   return (
     <div>
-      <Alert>
-        Hello <span>World</span>
-      </Alert>
+      <Button onClick={() => console.log("Clicked")} color="primary">
+        My Button
+      </Button>
     </div>
   );
 }
