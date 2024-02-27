6d43bf96ba8f720df179121209ddbd457cd699a8
yogeshbpatil <patilyogeshkumarb@gmail.com>
2024-02-27 17:08:55 +0530

    updating objects 5.6

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
 import Message from "./components/Message";
 
 function App() {
+  const [drink, setDrink] = useState({
+    title: "Americano",
+    price: 5,
+  });
+  const handleClick = () => {
+    const newDrink = { ...drink, price: 6 };
+    setDrink(newDrink);
+  };
   return (
-    <div>
-      <Message />
-    </div>
+    <>
+      {drink.price}
+      <div>
+        <button onClick={handleClick}>Click Me</button>
+      </div>
+    </>
   );
 }
 export default App;
