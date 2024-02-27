2ab007feeff88d8029a9d0d19a87154ba73a07c0
yogeshbpatil <patilyogeshkumarb@gmail.com>
2024-02-27 19:46:34 +0530

    simplifiying update loging with immer 5.10

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
 import { useState } from "react";
+import produce from "immer";
 
 function App() {
-  const [tags, setTags] = useState(["happy", "cheerful"]);
+  const [bugs, setBugs] = useState([
+    { id: 1, title: "Bug 1", fixed: false },
+    { id: 2, title: "Bug 2", fixed: false },
+  ]);
   const handleClick = () => {
-    //Add
-    setTags([...tags, "exciting"]);
-    //Remove
-    setTags(tags.filter((tags) => tags != "happy"));
-    //update
-    setTags(tags.map((tag) => (tag === "happy" ? "happiness" : tag)));
+    setBugs(
+      produce((draft) => {
+        const bug = draft.find((bug) => bug.id === 1);
+        if (bug) bug.fixed = true;
+      })
+    );
   };
   return (
     <>
       <div>
+        {bugs.map((bug) => (
+          <p key={bug.id}>
+            {bug.title} {bug.fixed ? "fixed" : "not Fixed"}
+          </p>
+        ))}
         <button onClick={handleClick}>Click Me</button>
       </div>
     </>
