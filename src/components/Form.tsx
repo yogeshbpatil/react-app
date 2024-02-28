84ca4dc14dc29f33bc6c0122ec45ac58e8a9175d
yogeshbpatil <patilyogeshkumarb@gmail.com>
2024-02-28 16:02:33 +0530

    managing forms with react hook forms 6.6 XX

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,8 +1,15 @@
 import React, { FormEvent, useRef, useState } from "react";
 import { FieldValues, useForm } from "react-hook-form";
-
+interface FormDate {
+  name: string;
+  age: number;
+}
 const Form = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
   const onSubmit = (data: FieldValues) => console.log(data);
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
@@ -11,11 +18,17 @@ const Form = () => {
           Name
         </label>
         <input
-          {...register("name")}
+          {...register("name", { required: true, minLength: 3 })}
           id="name"
           type="text"
           className="form-control"
         />
+        {errors.name?.type === "required" && (
+          <p className="text-danger">The name field is requied</p>
+        )}
+        {errors.age?.type === "minLength" && (
+          <p>The name must be at least 3 characters</p>
+        )}
       </div>
       <div className="mb-3">
         <label htmlFor="" className="form-label">
