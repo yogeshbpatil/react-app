e36567a2ecd83b793f6955d3165af44888119c2d
yogeshbpatil <patilyogeshkumarb@gmail.com>
2024-02-29 16:02:49 +0530

    working with async and wait 7.8

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,40 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { useEffect, useState } from "react";
+
 interface User {
   id: number;
   name: string;
 }
+
 function App() {
-  const [user, setusers] = useState<User[]>([]);
+  const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState("");
+
   useEffect(() => {
-    axios
-      .get<User[]>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => setusers(res.data))
-      .catch((err) => setError(err.message));
+    const fetchUsers = async () => {
+      try {
+        const res = await axios.get<User[]>(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        setUsers(res.data);
+      } catch (err) {
+        setError((err as AxiosError).message);
+      }
+    };
+
+    fetchUsers();
   }, []);
+
   return (
     <>
       {error && <p className="text-danger">{error}</p>}
       <ul>
-        {user.map((user) => (
+        {users.map((user) => (
           <li key={user.id}>{user.name}</li>
         ))}
       </ul>
     </>
   );
 }
+
 export default App;
