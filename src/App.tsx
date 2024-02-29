ba57682ead722e8c74bf00cecd51f59ed6871072
yogeshbpatil <patilyogeshkumarb@gmail.com>
2024-02-29 14:28:57 +0530

    Effect Dependency 7.3

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import { useEffect, useRef } from "react";
+import { useState } from "react";
+import ProductList from "./components/ProductList";
 
 function App() {
-  const ref = useRef<HTMLInputElement>(null);
-  useEffect(() => {
-    if (ref.current) ref.current.focus();
-  });
-  useEffect(() => {
-    document.title = "My App";
-  });
+  const [category, setCategory] = useState("");
   return (
     <div>
-      <input ref={ref} type="text" className="form-control"></input>
+      <select
+        className="form-select"
+        onChange={(event) => setCategory(event.target.value)}
+      >
+        <option value=""></option>
+        <option value="Clothing">Clothing</option>
+        <option value="HouseHolds">HouseHolds</option>
+      </select>
+      <ProductList category={category} />
     </div>
   );
 }
