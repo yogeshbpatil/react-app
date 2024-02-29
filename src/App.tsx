5670a6cd717b40b32703afe74f4dd1230031d117
yogeshbpatil <patilyogeshkumarb@gmail.com>
2024-02-29 16:43:07 +0530

    showing a loader indicator 7.70

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,34 @@
-import axios from "axios";
+import axios, { CanceledError } from "axios";
 import { useEffect, useState } from "react";
 interface User {
   id: number;
   name: string;
 }
+
 function App() {
   const [user, setusers] = useState<User[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
   useEffect(() => {
+    const controller = new AbortController();
+    setLoading(true);
     axios
       .get<User[]>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => setusers(res.data))
-      .catch((err) => setError(err.message));
-  }, []);
+      .then((res) => {
+        setusers(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setLoading(false);
+      });
+  });
+
   return (
     <>
       {error && <p className="text-danger">{error}</p>}
+      {isLoading && <div className="spinner-border"></div>}
       <ul>
         {user.map((user) => (
           <li key={user.id}>{user.name}</li>
