22137a118c839d89ee63aeae2afbb56b8987fe3e
yogeshbpatil <patilyogeshkumarb@gmail.com>
2024-02-29 21:17:40 +0530

    chore: _1

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,21 @@ import { useState } from "react";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
 import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
 import ExpenseForm from "./expense-tracker/components/ExpenseForm";
-// import categories from "./expense-tracker/categories";
 function App() {
   const [selectedCategory, setSelectedCategory] = useState("");
   const [expenses, setExpenses] = useState([
-    { id: 1, description: "alpha", amount: 10, category: "Utilities" },
-    { id: 2, description: "beta", amount: 20, category: "Utilities" },
-    { id: 3, description: "gama", amount: 30, category: "Utilities" },
-    { id: 4, description: "xyz", amount: 40, category: "Utilities" },
-    { id: 5, description: "pqr", amount: 50, category: "Geoceries" },
+    { id: 1, description: "Fan", amount: 10, category: "Utilities" },
+    { id: 2, description: "Bulb", amount: 20, category: "Utilities" },
+    { id: 3, description: "Table", amount: 30, category: "Utilities" },
+    { id: 4, description: "Chair", amount: 40, category: "Utilities" },
+    { id: 5, description: "Milk", amount: 50, category: "Groceries" },
+    { id: 6, description: "T.V.", amount: 50, category: "Entertainment" },
+    { id: 7, description: "Music", amount: 50, category: "Entertainment" },
+    { id: 8, description: "Mobile", amount: 50, category: "Entertainment" },
+    { id: 9, description: "Caram", amount: 50, category: "Entertainment" },
+    { id: 10, description: "Computer", amount: 50, category: "Entertainment" },
+    { id: 11, description: "Water", amount: 50, category: "Groceries" },
+    { id: 12, description: "Pulses", amount: 50, category: "Groceries" },
   ]);
   const visibleExpenses = selectedCategory
     ? expenses.filter((e) => e.category === selectedCategory)
